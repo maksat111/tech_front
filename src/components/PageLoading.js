295bb761ed702b6e4920183fe1cc838c544ee213
maksat111 <maksatakmyradow111@gmail.com>
2024-01-08 01:29:24 +0500

    some changes

--- src/components/PageLoading.js
+++ src/components/PageLoading.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-import { BeatLoader } from 'react-spinners';
-import Logo from '../images/logo.svg';
-import { isLogin } from '../utils/isLogin';
-import './PageLoading.css';
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { BeatLoader } from "react-spinners";
+import Logo from "../images/logo.svg";
+import { isLogin } from "../utils/isLogin";
+import "./PageLoading.css";
 
 function PageLoading() {
-    const location = useLocation();
-    if (!isLogin() && window.location.pathname !== '/') {
-        return <Navigate to="/" replace state={{ from: location }} />;
-    }
-    return (
-        <div className='pageLoading-container'>
-            <img src={Logo} alt='Turkmen Express' />
-            <BeatLoader
-                color={'blue'}
-                loading={true}
-                width={'400px'}
-                style={{ marginTop: '-50px' }}
-            />
-        </div>
-    );
+  const location = useLocation();
+  // if (!isLogin() && window.location.pathname !== '/') {
+  //     return <Navigate to="/" replace state={{ from: location }} />;
+  // }
+  return (
+    <div className="pageLoading-container">
+      <img src={Logo} alt="Turkmen Express" />
+      <BeatLoader
+        color={"blue"}
+        loading={true}
+        width={"400px"}
+        style={{ marginTop: "-50px" }}
+      />
+    </div>
+  );
 }
 
 export default PageLoading;
